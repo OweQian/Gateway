5f982e9efad72d71efd5120ff910a0c6c41b69a8
OweQian <1510106069@qq.com>
2023-04-10 16:59:09 +0800

    feat: 自定义 YAML

---
+++ src/utils/index.ts
@@ -0,0 +1,14 @@
+import { parse } from 'yaml';
+const path = require('path');
+const fs = require('fs');
+
+export const getEnv = () => {
+  return process.env.RUNNING_ENV;
+};
+
+export const getConfig = () => {
+  const environment = getEnv();
+  const yamlPath = path.join(process.cwd(), `./.config/.${environment}.yaml`);
+  const file = fs.readFileSync(yamlPath, 'utf-8');
+  return parse(file);
+};

01b14d7714b23066ee75004b85e9c2a77a402ac3
OweQian <1510106069@qq.com>
2023-04-13 21:07:44 +0800

    fix: 飞书环境变量替换

--- src/helper/feishu/const.ts
+++ src/helper/feishu/const.ts
@@ -1,2 +1,2 @@
-export const APP_ID = 'cli_a4bfbc8de124900d';
-export const APP_SECRET = 'EzIxxebekSOXgskzjk5INbTEHxGWXCJ5';
+export const APP_ID = 'cli_a4bd4e89cdfbd00e';
+export const APP_SECRET = 'yNxWgcbhTX4nAXIH3Fuzdg8QWHVqjfCy';
